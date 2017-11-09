1477e879446f3edda3ef3de2f48f65c09ad93d9b
potato2003 <potato2003@gmail.com>
2017-11-09 15:33:39 +0900

    change buffer file format

--- lib/embulk/output/google_spreadsheets/write_buffer.rb
+++ lib/embulk/output/google_spreadsheets/write_buffer.rb
@@ -2,6 +2,7 @@
 
 require 'forwardable'
 require 'tempfile'
+require 'zlib'
 
 module Embulk
   module Output
@@ -11,10 +12,19 @@ module Embulk
         extend Forwardable
         include Enumerable
 
+        MAX_LEN = 2 ** 32 - 1
+
+        d_crc32 = 'V1'
+        d_marshal_length = 'V1'
+        RECORD_META_PACK_DIRECTIVES = [d_crc32, d_marshal_length].join
+
+        RECORD_META_SIZE = 4 + 4
+
         attr_reader :file
 
         def initialize
           @file = Tempfile.new(safe_class_name)
+          @file.binmode
         end
 
         def close
@@ -26,14 +36,28 @@ module Embulk
         end
 
         def write_record(r)
-          write(Marshal.dump(r) << "\n")
+          # buffer file - record format
+          #
+          # | Offset | Length | Expl
+          # |  0     | 4      | crc32 of marshal data
+          # |  8     | 4      | length of marshal data
+          # | 12     | ??     | marshal data (maximum size 2^32 − 1)
+          m_data  = Marshal.dump(r)
+          m_crc32 = Zlib.crc32(m_data)
+          m_len   = m_data.size
+
+          if MAX_LEN < m_len
+            raise StandardError.new("record size is too large")
+          end
+
+          write [m_crc32, m_len].pack(RECORD_META_PACK_DIRECTIVES)
+          write m_data
         end
 
         def each
           file.rewind
-          file.each do |serialized_record|
-            yield Marshal.restore(serialized_record)
-          end
+
+          yield read_record until file.eof?
         end
 
         def_delegators :@file, :write
@@ -42,6 +66,21 @@ module Embulk
         # retrun a class name safe for filesystem
         def safe_class_name()
           self.class.name.downcase.gsub(/[^\d\w]/, '_')
+        end
+
+        def read_record
+          crc32, length = read_meta
+          m_data = file.read(length)
+
+          if crc32 != Zlib.crc32(m_data)
+            raise StandardError.new("broken record. buffer_file location: #{file.path}")
+          end
+
+          Marshal.restore(m_data)
+        end
+
+        def read_meta
+          file.read(RECORD_META_SIZE).unpack(RECORD_META_PACK_DIRECTIVES)
         end
 
       end
